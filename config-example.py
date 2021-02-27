c11b6a41220add6ee3b5268fb15f3ee4f9f304ec
Mario Wu <mariusz.wincior@gmail.com>
2021-02-27 10:21:01 +0100

    Update config-example.py

--- config-example.py
+++ config-example.py
@@ -1,8 +1,11 @@
+# noinspection PyUnresolvedReferences
+from ext_amp_conf import ExternalAmplifierConfig
+
 # MQTT Server connection
-mqtt_server_ip = "192.168.1.1"
+mqtt_server_ip = "192.168.2.100"
 mqtt_server_port = 1883
-mqtt_user = "user"
-mqtt_pass = "pass"
+mqtt_user = "wini"
+mqtt_pass = "dupa"
 
 # mqtt topic
 
@@ -10,12 +13,12 @@ base_topic = "megaphone"
 
 # control external Amplifier with GPIO
 
-ext_amplifier_control = False
-delay_amplifier = 2
-delay_speakers = 1
-gpio_amplifier = 17
-gpio_speakers = 27
+ext_amplifier = None
+
+# ext_amplifier = ExternalAmplifierConfig(gpio_amplifier=17,gpio_speakers=27,delay_amplifier=2, delay_speakers=1)
+
 
 # Chimes
 mp3_filename = "output.mp3"
+
 chime_filename = "gong.wav"
